e4c38a2ca5a3d46ae3fff31cdb7cac3eaf77f8e9
Adrian Zabolitzki <adrianzab@gmail.com>
2023-11-20 12:07:26 -0700

    Add functionality for headlines network request

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,22 @@
+import { useEffect, useState } from 'react';
 import Header from '../Header/Header'
 import Headlines from '../Headlines/Headlines'
+import { getHeadlines } from '../../apiCalls';
 import './App.css'
 
 function App() {
 
+  const [headlines, setHeadlines] = useState('')
+  const [headlineError, setHedlineError] = useState('')
+
+  useEffect(() => {
+    getHeadlines()
+      .then(data => {
+        console.log('data: ', data)
+      })
+      .catch(error => console.log(error))
+  }, [])
+  
   return (
     <main className="App">
       <Header className='header'/>
