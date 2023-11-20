3b8fa9c3b66329b788df270a3af16f99800abbb8
Adrian Zabolitzki <adrianzab@gmail.com>
2023-11-20 12:57:52 -0700

    Render dummy headlines

---
+++ src/components/HeadlineCard/HeadlineCard.js
@@ -0,0 +1,13 @@
+import './HeadlineCard.css'
+
+const HeadlineCard = ({ title }) => {
+
+  return (
+    <div className='headlineCard'>
+      <h2>{title}</h2>
+    </div>
+    
+  )
+}
+
+export default HeadlineCard
