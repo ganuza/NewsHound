eb9f675a86a01fe7d66a7deaabaf8e6b08bd66f4
Adrian Zabolitzki <adrianzab@gmail.com>
2023-11-20 12:20:46 -0700

    Create props to pass headline data to HeadlineCard

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,14 +9,26 @@ function App() {
   const [headlines, setHeadlines] = useState('')
   const [headlineError, setHedlineError] = useState('')
 
-  useEffect(() => {
-    getHeadlines()
-      .then(data => {
-        console.log('data: ', data)
-      })
-      .catch(error => console.log(error))
-  }, [])
-  
+  // useEffect(() => {
+  //   getHeadlines()
+  //     .then(data => {
+  //       console.log('data: ', data)
+  //       setHeadlines(data.articles)
+  //     })
+  //     .catch(error => console.log(error))
+  // }, [])
+
+  const headlineCards = headlines.map((headline, index) => {
+    <HeadlineCard
+      id={index}
+      key={index}
+      source={headline.source.name}
+      title={headline.title}
+      description={headline.description}
+      imgUrl={headline.urlToImage}
+    />
+  })
+
   return (
     <main className="App">
       <Header className='header'/>
