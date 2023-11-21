6a0e1fa6c4c7dab32d0d639eb69a08a42fc1ddf1
Adrian Zabolitzki <adrianzab@gmail.com>
2023-11-21 14:26:04 -0700

    Add functionality for rendering selectedArticle

--- src/components/SelectedArticle/SelectedArticle.js
+++ src/components/SelectedArticle/SelectedArticle.js
@@ -1,9 +1,24 @@
+import { useParams } from 'react-router-dom'
 import './SelectedArticle.css'
 
-const SelectedArticle = () => {
+const SelectedArticle = ({ headlines }) => {
+  const {title} = useParams()
+  console.log('title: ', title)
+  console.log('headlines: ', headlines)
 
+  const selectedStory = headlines.find((story) => story.title === title)
+
+  console.log('selectedStory: ', selectedStory)
   return (
-    <h2>Hello from SelectedArticle</h2>
+    <div className='selected-article-cont'>
+      <h2>Hello from SelectedArticle</h2>
+      <h2>{selectedStory.title}</h2>
+      <img src={selectedStory.urlToImage}/>
+      <h3>{selectedStory.publishedAt.slice(0, 10)}</h3>
+      <p>{selectedStory.content}</p>
+      <p>{selectedStory.source.name}</p>
+    </div>
+    
   )
 }
 
